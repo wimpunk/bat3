08f52972b47775322653ba474ee17d932378be12
wimpunk <wimpunk@daa5ebf2-bc28-0410-8f5a-e722156d1c5e>
2007-04-02 13:45:36 +0000

    Initial upload

--- trunk/src/functions.c
+++ trunk/src/functions.c
@@ -7,4 +7,27 @@
  *
  * This file is intended to contain all the possible functions of the bat3
  * */
+
+void changeled(struct bat3* state) {
+	if (state->led == ON) {
+		logabba(L_MAX, "changeled: switching LED OFF");
+		state->led = OFF;
+	} else {
+		logabba(L_MAX, "changeled: switching LED ON");
+		state->led = ON;
+		
+	}
+}
+
+void doload(struct bat3* state) {
+
+	// OP AMP must be enabled for charging to work
+	state->opampEn  = ON;
+	state->PWM1en   = ON;
+	state->PWM2en   = ON;
+	state->buckEn   = OFF;
+	state->offsetEn = OFF;
+	state->pwm_lo   -= 0x10;
+	state->pwm_t    = 500;
+}
  
