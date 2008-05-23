b28ab63ef856744b1c3c86cf01f7754af0de2638
wimpunk <wimpunk@daa5ebf2-bc28-0410-8f5a-e722156d1c5e>
2008-05-23 11:54:55 +0000

    Modified identation

--- src/bat3.c
+++ src/bat3.c
@@ -41,193 +41,193 @@
 #define MAX_RETRIES 5
 
 char *print_onoff(onoff_t onoff) {
-    if (onoff == ON) {
-	return "ON";
-    } else {
-	return "OFF";
-    }
-    return NULL;
+	if (onoff == ON) {
+		return "ON";
+	} else {
+		return "OFF";
+	}
+	return NULL;
 }
 /*
- void writemsg(int fd, char* msg, int len, int read)
- {
-
-
- // OP AMP must be enabled for charging to work
- req.opampEn = 1;
- req._buckEn = 1;
- req._offsetEn = 1;
- req.checksum = calcCrc((char*)&req, sizeof(req)-1);
-
- cnt = write(fd, (void *)&req, sizeof(req));
-
- logabba(L_MAX, "Write returned %d", cnt);
-
- // OP AMP must be enabled for charging to work
- req.opampEn = 1;
- req.PWM1en = 1;
- req.PWM2en = 1;
- req._buckEn = 1;
- req._offsetEn = 1;
-
- req.outputs = rep->outputs;
- req.alarm   = 0;
- req.softJP3 = 0;
- req.pwm_lo  = rep->pwm_lo;
- req.pwm_t   = rep->pwm_t;
-
-
- return;
- }
+ * void writemsg(int fd, char* msg, int len, int read)
+ * {
+ *
+ *
+ * // OP AMP must be enabled for charging to work
+ * req.opampEn = 1;
+ * req._buckEn = 1;
+ * req._offsetEn = 1;
+ * req.checksum = calcCrc((char*)&req, sizeof(req)-1);
+ *
+ * cnt = write(fd, (void *)&req, sizeof(req));
+ *
+ * logabba(L_MAX, "Write returned %d", cnt);
+ *
+ * // OP AMP must be enabled for charging to work
+ * req.opampEn = 1;
+ * req.PWM1en = 1;
+ * req.PWM2en = 1;
+ * req._buckEn = 1;
+ * req._offsetEn = 1;
+ *
+ * req.outputs = rep->outputs;
+ * req.alarm   = 0;
+ * req.softJP3 = 0;
+ * req.pwm_lo  = rep->pwm_lo;
+ * req.pwm_t   = rep->pwm_t;
+ *
+ *
+ * return;
+ * }
  */
 
 static int getsample(int fd, struct bat3 *sample, FILE *logfile) {
-    
-    struct bat3 temp;
-    int cnt;
-    char msg[56];
-    int error=0;
-    int retry=1;
-    
-    
-    while (error<MAX_RETRIES && retry) {
 	
-	if (!(cnt=readStream(fd, msg, sizeof(msg)))) {
-	    logabba(L_NOTICE, "Could not readStream, error=%d, cnt=%d", error, cnt);
-	    error++;
-	    continue;
+	struct bat3 temp;
+	int cnt;
+	char msg[56];
+	int error=0;
+	int retry=1;
+	
+	
+	while (error<MAX_RETRIES && retry) {
+		
+		if (!(cnt=readStream(fd, msg, sizeof(msg)))) {
+			logabba(L_NOTICE, "Could not readStream, error=%d, cnt=%d", error, cnt);
+			error++;
+			continue;
+		}
+		
+		// if (logfile == NULL ) logabba(L_MIN, "Logfile == NULL");
+		if (decodemsg(msg, cnt, &temp, logfile)!=0) {
+			logabba(L_MIN, "Could not decodemsg, error=%d", error);
+			error++;
+			continue;
+		}
+		
+		retry=0;
+		
 	}
 	
-	// if (logfile == NULL ) logabba(L_MIN, "Logfile == NULL");
-	if (decodemsg(msg, cnt, &temp, logfile)!=0) {
-	    logabba(L_MIN, "Could not decodemsg, error=%d", error);
-	    error++;
-	    continue;
+	if (!retry) {
+		logabba(L_INFO, "Good sample after error=%d tries ", error);
+		*sample = temp;
+		return 1;
 	}
 	
-	retry=0;
+	logabba(L_MIN, "Could not get a sample after error=%d tries ", error);     ;
+	return 0;
 	
-    }
-    
-    if (!retry) {
-	logabba(L_INFO, "Good sample after error=%d tries ", error);
-	*sample = temp;
-	return 1;
-    }
-    
-    logabba(L_MIN, "Could not get a sample after error=%d tries ", error);     ;
-    return 0;
-    
 }
 
 static void usage(char *progname) {
-    printf("\n");
-    printf("Usage:\n");
-    printf("\n");
-    printf("   %s [-c current][-d device][-l loglevel][-r][-s samples][-w value]\n", progname);
-    printf("      -a address: address to use while reading/writing, default %i\n", DEFAULT_ADDRESS);
-    printf("      -c current: current to load (mA), default %i\n", DEFAULT_CURRENT);
-    printf("      -d device: device to use, default %s\n", BAT3DEV );
-    printf("      -f logfile: file to dump arrays to, default /dev/null\n");
-    printf("      -l loglevel: loglevel to use, default %i\n", DEFAULT_LOGLEVEL );
-    printf("      -p portnr: portnumber to listen to, default %i\n", DEFAULT_PORT);
-    printf("      -r : read from [address]\n");
-    printf("      -s samples: maxsamples to use, default %i\n", DEFAULT_SAMPLES );
-    printf("      -w value: write value to [address], default %i\n", DEFAULT_VALUE);
-    printf("\n");
-    printf("(bat3 release %s-r%s)\n", VERSION, REV);
-    printf("\n");
+	printf("\n");
+	printf("Usage:\n");
+	printf("\n");
+	printf("   %s [-c current][-d device][-l loglevel][-r][-s samples][-w value]\n", progname);
+	printf("      -a address: address to use while reading/writing, default %i\n", DEFAULT_ADDRESS);
+	printf("      -c current: current to load (mA), default %i\n", DEFAULT_CURRENT);
+	printf("      -d device: device to use, default %s\n", BAT3DEV );
+	printf("      -f logfile: file to dump arrays to, default /dev/null\n");
+	printf("      -l loglevel: loglevel to use, default %i\n", DEFAULT_LOGLEVEL );
+	printf("      -p portnr: portnumber to listen to, default %i\n", DEFAULT_PORT);
+	printf("      -r : read from [address]\n");
+	printf("      -s samples: maxsamples to use, default %i\n", DEFAULT_SAMPLES );
+	printf("      -w value: write value to [address], default %i\n", DEFAULT_VALUE);
+	printf("\n");
+	printf("(bat3 release %s-r%s)\n", VERSION, REV);
+	printf("\n");
 }
 
 /*
- static void flush(int fd) {
- int cnt=0;
- char c;
- while ((read(fd, &c, 1))>0) cnt++;
- logabba(L_MAX, "Flush was reading %d bytes", cnt);
- }
+ * static void flush(int fd) {
+ * int cnt=0;
+ * char c;
+ * while ((read(fd, &c, 1))>0) cnt++;
+ * logabba(L_MAX, "Flush was reading %d bytes", cnt);
+ * }
  */
 
 
 int doRound(int fd, struct bat3 *sample, int current, FILE *logfile) {
-    
-    struct bat3 state = *sample;
-    int cnt;
-    char msg[56];
-    // while ((cntsamples<samples) || (samples == -1))	{
-    
-    // TODO: quick hack
-    // if ((cntsamples == 0))
-    state.softJP3 = ON;
-    
-    logabba(L_NOTICE, "Original led: %s", print_onoff(state.led));
-    // Even when running on batteries, we have to kietel the opamp
-    if (state.batRun == ON) {
-	state.led = OFF;
-	doload(&state, current);
-    } else {
-	changeled(&state);
-	doload(&state, current);
-	logabba(L_MAX, "Switching led to %s", print_onoff(state.led));
-    }
-    logabba(L_NOTICE, "will encode led: %s", print_onoff(state.led));
-    if ((cnt = encodemsg(msg, sizeof(msg), &state))) {
-	logabba(L_INFO, "Writing msg");
-	writeStream(fd, msg, cnt);
-    }
-    
-    cnt=0;
-    
-    //    prevstate = state;
-    while (!getsample(fd, &state, logfile) && cnt<MAX_RETRIES) {
-	cnt++;
-    }
-    
-    if (cnt>=MAX_RETRIES) {
-	logabba(L_MIN, "Did not get a sample even after retrying %d times", cnt);
-	return -1;
-    }
-        logabba(L_NOTICE, "got state led: %s", print_onoff(state.led));
-    *sample = state;
-    logabba(L_NOTICE, "got sample led: %s", print_onoff(sample->led));
-    
-    // usleep(100);
-    
-    return 0;
-    
+	
+	struct bat3 state = *sample;
+	int cnt;
+	char msg[56];
+	// while ((cntsamples<samples) || (samples == -1))	{
+	
+	// TODO: quick hack
+	// if ((cntsamples == 0))
+	state.softJP3 = ON;
+	
+	logabba(L_NOTICE, "Original led: %s", print_onoff(state.led));
+	// Even when running on batteries, we have to kietel the opamp
+	if (state.batRun == ON) {
+		state.led = OFF;
+		doload(&state, current);
+	} else {
+		changeled(&state);
+		doload(&state, current);
+		logabba(L_MAX, "Switching led to %s", print_onoff(state.led));
+	}
+	logabba(L_NOTICE, "will encode led: %s", print_onoff(state.led));
+	if ((cnt = encodemsg(msg, sizeof(msg), &state))) {
+		logabba(L_INFO, "Writing msg");
+		writeStream(fd, msg, cnt);
+	}
+	
+	cnt=0;
+	
+	//    prevstate = state;
+	while (!getsample(fd, &state, logfile) && cnt<MAX_RETRIES) {
+		cnt++;
+	}
+	
+	if (cnt>=MAX_RETRIES) {
+		logabba(L_MIN, "Did not get a sample even after retrying %d times", cnt);
+		return -1;
+	}
+	logabba(L_NOTICE, "got state led: %s", print_onoff(state.led));
+	*sample = state;
+	logabba(L_NOTICE, "got sample led: %s", print_onoff(sample->led));
+	
+	// usleep(100);
+	
+	return 0;
+	
 }
 
 int main(int argc, char *argv[]) {
-    int c, fd;
-    char device[50]=""; // device to use
-    
-    int errcnt=0;
-    
-    //    int cnt,
-    int cntsamples;
-    
-    int loglevel = DEFAULT_LOGLEVEL;
-    int samples  = DEFAULT_SAMPLES;
-    int current  = DEFAULT_CURRENT;
-    int address  = DEFAULT_ADDRESS;
-    int portno   = DEFAULT_PORT;
-    
-    
-    int read  = 0;
-    int write = 0;
-    int value = 0;
-    
-    int socketfd = 0;
-    
-    FILE *logfile = NULL;
-    
-    struct bat3 state, prevstate;
-    
-    strncpy(device, BAT3DEV, sizeof(BAT3DEV));
-    
+	int c, fd;
+	char device[50]=""; // device to use
+	
+	int errcnt=0;
+	
+	//    int cnt,
+	int cntsamples;
+	
+	int loglevel = DEFAULT_LOGLEVEL;
+	int samples  = DEFAULT_SAMPLES;
+	int current  = DEFAULT_CURRENT;
+	int address  = DEFAULT_ADDRESS;
+	int portno   = DEFAULT_PORT;
+	
+	
+	int read  = 0;
+	int write = 0;
+	int value = 0;
+	
+	int socketfd = 0;
+	
+	FILE *logfile = NULL;
+	
+	struct bat3 state, prevstate;
+	
+	strncpy(device, BAT3DEV, sizeof(BAT3DEV));
+	
 	initMySocket();
-    
-    while ((c=getopt(argc, argv, "a:c:d:f:h?l:p:rs:w:"))!=EOF) {
+	
+	while ((c=getopt(argc, argv, "a:c:d:f:h?l:p:rs:w:"))!=EOF) {
 		switch (c) {
 			case 'a': // address
 				address=atoi(optarg);
