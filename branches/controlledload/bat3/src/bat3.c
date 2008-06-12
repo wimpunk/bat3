20acfce600b90ea202e66ea8e01cb8f254a899fd
wimpunk <wimpunk@daa5ebf2-bc28-0410-8f5a-e722156d1c5e>
2008-06-12 13:35:31 +0000

    Little restyling

--- branches/controlledload/bat3/src/bat3.c
+++ branches/controlledload/bat3/src/bat3.c
@@ -40,6 +40,12 @@
 
 int current  = DEFAULT_CURRENT;
 
+typedef enum {
+	E_OKAY,
+	E_DEVICE,
+	E_SAMPLE
+} errors;
+
 char *print_onoff(onoff_t onoff) {
 	if (onoff == ON) {
 		return "ON";
@@ -48,6 +54,8 @@ char *print_onoff(onoff_t onoff) {
 	}
 	return NULL;
 }
+
+int doRound(int fd, struct bat3 *sample, int current, FILE *logfile);
 
 int setCurrent(int i) {
 	
@@ -137,6 +145,180 @@ static void usage(char *progname) {
 	
 }
 
+static void initParams(struct action *params) {
+	// init params
+	params->address  = DEFAULT_ADDRESS;
+	params->current  = DEFAULT_CURRENT;
+	strncpy(params->device, BAT3DEV, sizeof(BAT3DEV));
+	params->read		= 0;
+	params->write	= 0;
+	params->value	= 0;
+	params->logfile	= NULL;
+	params->loglevel = DEFAULT_LOGLEVEL;
+	params->samples  = DEFAULT_SAMPLES;
+	params->portno	= DEFAULT_PORT;
+}
+
+static int processArguments(int argc, char **argv, struct action *params) {
+	
+	int c;
+	int errcnt=0;
+	
+	while ((c=getopt(argc, argv, "a:c:d:f:h?l:p:rs:w:"))!=EOF) {
+		switch (c) {
+			case 'a': // address
+				params->address=atoi(optarg);
+				break;
+			case 'c': // current
+				setCurrent(atoi(optarg));
+				break;
+			case 'd': // device
+				strncpy(params->device, optarg, sizeof(params->device)-1);
+				printf("I'll read device %s\n", params->device);
+				break;
+			case 'f': // logfile
+				params->logfile = fopen(optarg, "w");
+				if (params->logfile == NULL) {
+					fprintf(stderr, "Error opening logfile %s: %s\n", optarg, strerror(errno));
+				}
+			case 'l': // loglevel
+				params->loglevel = atoi(optarg);
+				break;
+			case 'p': // portnr to listen to
+				params->portno = atoi(optarg);
+				break;
+			case 'r': // read
+				params->read=1;
+				break;
+			case 's': // samples
+				params->samples = atoi(optarg);
+				break;
+			case 'w':
+				params->write = 1;
+				params->value = atoi(optarg);
+				break;
+				
+			case '?':
+			case 'h':
+			default:
+				errcnt++;
+				
+		}
+		if (errcnt) break;
+	}
+	
+	if (errcnt) {
+		usage(argv[0]);
+		return(1);
+	}
+	
+	return 0;
+	
+}
+
+int runArgument(struct action *params) {
+	
+	
+	int				fd;
+	int				FlushTime = 20 * 1000; // 20 characters @ 9600bps
+	fd_set			rfds;
+	struct timeval  tv;
+	int				sw_continue = 1;
+	struct timeval  now, lastrun;
+	struct bat3		state, prevstate;
+	int				cntsamples=0;
+	int				socketfd = 0;
+	
+	if ((fd = openStream(params->device)) == -1) {
+		logabba(L_MIN, "Could not open device %s", params->device);
+		return E_DEVICE;
+	}
+	
+	// initialising
+	// we need an inital value so we get a sample
+	if (!getsample(fd, &state, params->logfile)) {
+		logabba(L_MIN, "Did not get a sample");
+		return E_SAMPLE;
+	}
+	
+	
+	// ignoring broken pipes
+	signal(SIGPIPE, SIG_IGN);
+	
+	
+	while (((cntsamples<params->samples) || (params->samples == -1)) && (sw_continue))	{
+		
+		FD_ZERO( &rfds );
+		FD_SET( fd, &rfds );
+		
+		tv.tv_sec  = FlushTime/1000000;
+		tv.tv_usec = FlushTime%1000000;
+		
+		int val;
+		
+		if (select( FD_SETSIZE, &rfds, NULL, NULL, &tv ) < 0) {
+			fprintf(stderr, "Select error: %s\n", strerror(errno));
+			return -1;
+		}
+		
+		if (FD_ISSET(fd, &rfds)) {
+			
+			if (params->read) doread(&state, params->address);
+			if (params->write) dowrite(&state, params->address, params->value);
+			
+			gettimeofday(&lastrun, NULL); //needed to calculate the wait time
+			
+			// TODO: we should verify the value of doRound.  When it returns
+			// -1 it can't communicate with the TS-BAT3
+			doRound(fd, &state, current, params->logfile);
+			if (!((params->samples == -1) && (cntsamples>10000))) cntsamples++;
+			
+			
+			if (params->read && cntsamples==3) {
+				logabba(L_MIN, "Reading from address %i: %04X=%04X",
+						params->address, state.ee_addr, state.ee_data);
+			}
+			
+			// checking switched states
+			if (state.batRun != prevstate.batRun) {
+				logabba(L_MIN, "Running on %s",
+						state.batRun==ON?"batteries":"current" );
+			}
+			
+			if (state.softJP3 != prevstate.softJP3) {
+				logabba(L_MIN, "Soft JP3 switched to %s after %i samples",
+						print_onoff(state.softJP3), cntsamples );
+			}
+			
+			// setting waittime for select
+			tv.tv_sec  = FlushTime/1000000;
+			tv.tv_usec = FlushTime%1000000;
+			
+		}
+		
+		
+		prevstate = state;
+		setBatState(&state);
+		
+		// Processing socket connections
+		if (sw_continue) {
+			sw_continue = MYSOCK_END != processMySocket(socketfd);
+		}
+		
+		gettimeofday(&now, NULL);
+		
+		val = 25 * 1000 - ( now.tv_sec - lastrun.tv_sec ) * 1000 * 1000 -  ( now.tv_usec - lastrun.tv_usec );
+		if (val <= 0 ) val = 0;
+		usleep(val);
+		
+	}
+	
+	closeMySocket(socketfd);
+	close(fd);
+	
+	return E_OKAY;
+}
+
 /*
  * doRound creates a structure based on the current state found in sample
  * and writes the result to the TS-BAT3 which should return a new sample.
@@ -192,184 +374,23 @@ int doRound(int fd, struct bat3 *sample, int current, FILE *logfile) {
 
 int main(int argc, char *argv[]) {
 	
-	int c, fd;
-	char device[50]=""; // device to use
-	
-	int errcnt=0;
-	
-	int cntsamples;
-	
-	int loglevel = DEFAULT_LOGLEVEL;
-	int samples  = DEFAULT_SAMPLES;
-	
-	int address  = DEFAULT_ADDRESS;
-	int portno   = DEFAULT_PORT;
-	
-	int read  = 0;
-	int write = 0;
-	int value = 0;
-	
-	int socketfd = 0;
-	
-	FILE *logfile = NULL;
-	
-	struct bat3 state, prevstate;
-	
-	strncpy(device, BAT3DEV, sizeof(BAT3DEV));
+	struct action	params;	
 	
 	initMySocket();
+	initParams(&params);
+	processArguments(argc, argv, &params);
+	setPortno(params.portno);
 	
-	while ((c=getopt(argc, argv, "a:c:d:f:h?l:p:rs:w:"))!=EOF) {
-		switch (c) {
-			case 'a': // address
-				address=atoi(optarg);
-				break;
-			case 'c': // current
-				setCurrent(atoi(optarg));
-				break;
-			case 'd': // device
-				strncpy(device, optarg, sizeof(device)-1);
-				printf("I'll read device %s\n", device);
-				break;
-			case 'f': // logfile
-				logfile = fopen(optarg, "w");
-				if (logfile == NULL) {
-					fprintf(stderr, "Error opening logfile %s: %s\n", optarg, strerror(errno));
-				}
-			case 'l': // loglevel
-				loglevel = atoi(optarg);
-				break;
-			case 'p': // pipe
-				portno = atoi(optarg);
-				break;
-			case 'r': // read
-				read=1;
-				break;
-			case 's': // samples
-				samples = atoi(optarg);
-				break;
-			case 'w':
-				write = 1;
-				value = atoi(optarg);
-				break;
-				
-			case '?':
-			case 'h':
-			default:
-				errcnt++;
-				
-		}
-		if (errcnt) break;
-	}
+	if ((params.samples<-1)) params.samples = DEFAULT_SAMPLES;
 	
-	if (errcnt) {
-		usage(argv[0]);
-		return(1);
-	}
-	
-	
-	if ((fd = openStream(device)) == -1) {
-		logabba(L_MIN, "Could not open device %s", device);
-		return 0;
-	}
-	
-	setPortno(portno);
-	
-	if ((samples<-1)) samples = DEFAULT_SAMPLES;
-	
-	setloglevel(loglevel, "bat3");
+	setloglevel(params.loglevel, "bat3");
 	
 	logabba(L_MIN, "%s (Ver %s) started, loglevel %i, getting %d samples, using %imA to load, listening to port %d",
-			argv[0], VERSION, loglevel, samples, current, portno);
+			argv[0], VERSION, params.loglevel, params.samples, params.current, params.portno);
 	
-	// TODO: this should be done in a separate function
+	runArgument(&params);
 	
-	// initialising 
-	// we need an inital value so we get a sample
-	if (!getsample(fd, &state, logfile)) {
-		logabba(L_MIN, "Did not get a sample");
-		return 0;
-	}
 	
-	cntsamples=0;
-	// ignoring broken pipes
-	signal(SIGPIPE, SIG_IGN);
-	
-	int		    FlushTime = 20 * 1000; // 20 characters @ 9600bps
-	fd_set	    rfds;
-	struct timeval  tv;
-	int		    sw_continue = 1;
-	struct timeval  now, lastrun;
-	
-	while (((cntsamples<samples) || (samples == -1)) && (sw_continue))	{
-		
-		FD_ZERO( &rfds );
-		FD_SET( fd, &rfds );
-		
-		tv.tv_sec  = FlushTime/1000000;
-		tv.tv_usec = FlushTime%1000000;
-		
-		int val;
-		
-		if (select( FD_SETSIZE, &rfds, NULL, NULL, &tv ) < 0) {
-			fprintf(stderr, "Select error: %s\n", strerror(errno));
-			return -1;
-		}
-		
-		if (FD_ISSET(fd, &rfds)) {
-			
-			if (read) doread(&state, address);
-			if (write) dowrite(&state, address, value);
-			
-			gettimeofday(&lastrun, NULL); //needed to calculate the wait time
-			
-			// TODO: we should verify the value of doRound.  When it returns
-			// -1 it can't communicate with the TS-BAT3
-			doRound(fd, &state, current, logfile);
-			if (!((samples == -1) && (cntsamples>10000))) cntsamples++;
-			
-			
-			if (read && cntsamples==3) {
-				logabba(L_MIN, "Reading from address %i: %04X=%04X", 
-						address, state.ee_addr, state.ee_data);
-			}
-			
-			// checking switched states
-			if (state.batRun != prevstate.batRun) {
-				logabba(L_MIN, "Running on %s", 
-						state.batRun==ON?"batteries":"current" );
-			}
-			
-			if (state.softJP3 != prevstate.softJP3) {
-				logabba(L_MIN, "Soft JP3 switched to %s after %i samples", 
-						print_onoff(state.softJP3), cntsamples );
-			}
-			
-			// setting waittime for select
-			tv.tv_sec  = FlushTime/1000000;
-			tv.tv_usec = FlushTime%1000000;
-			
-		}
-		
-		
-		prevstate = state;
-		setBatState(&state);
-		
-		// Processing socket connections
-		if (sw_continue) {
-			sw_continue = MYSOCK_END != processMySocket(socketfd);
-		}
-		
-		gettimeofday(&now, NULL);
-		
-		val = 25 * 1000 - ( now.tv_sec - lastrun.tv_sec ) * 1000 * 1000 -  ( now.tv_usec - lastrun.tv_usec );
-		if (val <= 0 ) val = 0;
-		usleep(val);
-
-	}
-	
-	close(fd);
-	closeMySocket(socketfd);
 	
 	return 0;
 	
