e022222d173b8d972c07514c4df759be1ef491a3
wimpunk <wimpunk@daa5ebf2-bc28-0410-8f5a-e722156d1c5e>
2008-06-18 17:43:27 +0000

    Merged with colledload branch

--- src/bat3func.h
+++ src/bat3func.h
@@ -13,26 +13,13 @@ void changeled(struct bat3* state);
 // should be changed by one do-action function
 // which uses the current state and modifies it
 // to the wanted state
-void doload(struct bat3* state, int target); 
+void doload(struct bat3* state, struct action* target);
 void doread(struct bat3* state, int address);
 void dowrite(struct bat3* state, int address, int value);
 
 void setBatState(struct bat3* newState);
 int getBatI();
-struct bat3* getState();	
+struct bat3* getState();
 onoff_t getBatRun();
 
-struct action {
-	
-/* this should contain the action we want to take:
- * load
- * read
- * write
- */
-	int current;
-	int read;
-	int write;
-	int address;
-	
-};
 #endif
