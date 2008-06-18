f8740edc903f2962370facfa3b6650413f889f80
wimpunk <wimpunk@daa5ebf2-bc28-0410-8f5a-e722156d1c5e>
2008-06-18 17:43:27 +0000

    Merged with colledload branch

--- trunk/src/bat3.c
+++ trunk/src/bat3.c
@@ -38,7 +38,17 @@
 #define DEFAULT_PORT     1302
 #define MAX_RETRIES 5
 
-int current  = DEFAULT_CURRENT;
+
+
+
+
+typedef enum {
+	E_OKAY,
+	E_DEVICE,
+	E_SAMPLE
+} errors;
+
+struct action	params;	
 
 char *print_onoff(onoff_t onoff) {
 	if (onoff == ON) {
@@ -49,29 +59,33 @@ char *print_onoff(onoff_t onoff) {
 	return NULL;
 }
 
+int doRound(int fd, struct bat3 *sample, struct action *params);
+
 int setCurrent(int i) {
 	
-	if ((i<0) || (i>2000)) current = DEFAULT_CURRENT;
-	else current = i;
+	if ((i<0) || (i>2000)) params.current = DEFAULT_CURRENT;
+	else params.current = i;
 	
-	return current;
+	return params.current;
 	
 }
 
 int getCurrent() {
 	
-	return current;
+	return params.current;
 	
 }
 
 int setLoglevel(int i) {
+	
 	setloglevel(i, "bat3");
+	params.loglevel=getloglevel();
 	
 	return i;
 	
 }
 
-int getLoglevel() {
+int getLoglevel(struct action *params) {
 	
 	return getloglevel();
 	
@@ -137,120 +151,60 @@ static void usage(char *progname) {
 	
 }
 
-/*
- * doRound creates a structure based on the current state found in sample
- * and writes the result to the TS-BAT3 which should return a new sample.
- * TODO: it should also contain the reading and writing to eeprom.
- */
-int doRound(int fd, struct bat3 *sample, int current, FILE *logfile) {
+static void initParams(struct action *params) {
+	// init params
+	params->address  = DEFAULT_ADDRESS;
+	params->current  = DEFAULT_CURRENT;
+	strncpy(params->device, BAT3DEV, sizeof(BAT3DEV));
+	params->read		= 0;
+	params->write	= 0;
+	params->value	= 0;
+	params->logfile	= NULL;
+	params->loglevel = DEFAULT_LOGLEVEL;
+	params->samples  = DEFAULT_SAMPLES;
+	params->portno	= DEFAULT_PORT;
 	
-	struct bat3 state = *sample;
-	int cnt;
-	char msg[56];
-	
-	
-	logabba(L_INFO, "Original led: %s", print_onoff(state.led));
-	// Even when running on batteries, we have to kietel the opamp
-	// TODO: is this still correct?  I think we just have to write
-	// some info to the battery within 2 minutes and wait for reaction.
-	if (state.batRun == ON) {
-		state.led = OFF;
-		doload(&state, current);
-	} else {
-		changeled(&state);
-		// TODO: we should pass more info about the current loading state
-		// to the battery
-		doload(&state, current);
-		logabba(L_NOTICE, "Switching led to %s", print_onoff(state.led));
-	}
-	
-	logabba(L_INFO, "will encode led: %s", print_onoff(state.led));
-	
-	if ((cnt = encodemsg(msg, sizeof(msg), &state))) {
-		logabba(L_INFO, "Writing msg");
-		writeStream(fd, msg, cnt);
-	}
-	
-	cnt=0;
-	
-	while (!getsample(fd, &state, logfile) && cnt<MAX_RETRIES) {
-		cnt++;
-	}
-	
-	if (cnt>=MAX_RETRIES) {
-		logabba(L_MIN, "Did not get a sample even after retrying %d times", cnt);
-		return -1;
-	}
-	
-	logabba(L_INFO, "got state led: %s", print_onoff(state.led));
-	*sample = state;
-	logabba(L_INFO, "got sample led: %s", print_onoff(sample->led));
-	
-	return 0;
-	
+// 	params->weightcnt = 0;
+	params->weightpos = 0;
 }
 
-int main(int argc, char *argv[]) {
+static int processArguments(int argc, char **argv, struct action *params) {
 	
-	int c, fd;
-	char device[50]=""; // device to use
-	
+	int c;
 	int errcnt=0;
-	
-	int cntsamples;
-	
-	int loglevel = DEFAULT_LOGLEVEL;
-	int samples  = DEFAULT_SAMPLES;
-	
-	int address  = DEFAULT_ADDRESS;
-	int portno   = DEFAULT_PORT;
-	
-	int read  = 0;
-	int write = 0;
-	int value = 0;
-	
-	int socketfd = 0;
-	
-	FILE *logfile = NULL;
-	
-	struct bat3 state, prevstate;
-	
-	strncpy(device, BAT3DEV, sizeof(BAT3DEV));
-	
-	initMySocket();
 	
 	while ((c=getopt(argc, argv, "a:c:d:f:h?l:p:rs:w:"))!=EOF) {
 		switch (c) {
 			case 'a': // address
-				address=atoi(optarg);
+				params->address=atoi(optarg);
 				break;
 			case 'c': // current
 				setCurrent(atoi(optarg));
 				break;
 			case 'd': // device
-				strncpy(device, optarg, sizeof(device)-1);
-				printf("I'll read device %s\n", device);
+				strncpy(params->device, optarg, sizeof(params->device)-1);
+				printf("I'll read device %s\n", params->device);
 				break;
 			case 'f': // logfile
-				logfile = fopen(optarg, "w");
-				if (logfile == NULL) {
+				params->logfile = fopen(optarg, "w");
+				if (params->logfile == NULL) {
 					fprintf(stderr, "Error opening logfile %s: %s\n", optarg, strerror(errno));
 				}
 			case 'l': // loglevel
-				loglevel = atoi(optarg);
+				params->loglevel = atoi(optarg);
 				break;
-			case 'p': // pipe
-				portno = atoi(optarg);
+			case 'p': // portnr to listen to
+				params->portno = atoi(optarg);
 				break;
 			case 'r': // read
-				read=1;
+				params->read=1;
 				break;
 			case 's': // samples
-				samples = atoi(optarg);
+				params->samples = atoi(optarg);
 				break;
 			case 'w':
-				write = 1;
-				value = atoi(optarg);
+				params->write = 1;
+				params->value = atoi(optarg);
 				break;
 				
 			case '?':
@@ -267,41 +221,42 @@ int main(int argc, char *argv[]) {
 		return(1);
 	}
 	
+	return 0;
 	
-	if ((fd = openStream(device)) == -1) {
-		logabba(L_MIN, "Could not open device %s", device);
-		return 0;
+}
+
+int runArgument(struct action *params) {
+	
+	
+	int				fd;
+	int				FlushTime = 20 * 1000; // 20 characters @ 9600bps
+	fd_set			rfds;
+	struct timeval  tv;
+	int				sw_continue = 1;
+	struct timeval  now, lastrun;
+	struct bat3		state, prevstate;
+	int				cntsamples=0;
+	int				socketfd = 0;
+	
+	if ((fd = openStream(params->device)) == -1) {
+		logabba(L_MIN, "Could not open device %s", params->device);
+		return E_DEVICE;
 	}
 	
-	setPortno(portno);
-	
-	if ((samples<-1)) samples = DEFAULT_SAMPLES;
-	
-	setloglevel(loglevel, "bat3");
-	
-	logabba(L_MIN, "%s (Ver %s) started, loglevel %i, getting %d samples, using %imA to load, listening to port %d",
-			argv[0], VERSION, loglevel, samples, current, portno);
-	
-	// TODO: this should be done in a separate function
-	
-	// initialising 
+	// initialising
 	// we need an inital value so we get a sample
-	if (!getsample(fd, &state, logfile)) {
+	if (!getsample(fd, &state, params->logfile)) {
 		logabba(L_MIN, "Did not get a sample");
-		return 0;
+		return E_SAMPLE;
 	}
 	
-	cntsamples=0;
+	// TODO: dont think we need this
 	// ignoring broken pipes
 	signal(SIGPIPE, SIG_IGN);
 	
-	int		    FlushTime = 20 * 1000; // 20 characters @ 9600bps
-	fd_set	    rfds;
-	struct timeval  tv;
-	int		    sw_continue = 1;
-	struct timeval  now, lastrun;
 	
-	while (((cntsamples<samples) || (samples == -1)) && (sw_continue))	{
+	while (((cntsamples<params->samples) || (params->samples == -1)) 
+			&& (sw_continue))	{
 		
 		FD_ZERO( &rfds );
 		FD_SET( fd, &rfds );
@@ -318,30 +273,34 @@ int main(int argc, char *argv[]) {
 		
 		if (FD_ISSET(fd, &rfds)) {
 			
-			if (read) doread(&state, address);
-			if (write) dowrite(&state, address, value);
+			int oldpos;
+			int newpos;
+			
+			if (params->read) doread(&state, params->address);
+			if (params->write) dowrite(&state, params->address, params->value);
 			
 			gettimeofday(&lastrun, NULL); //needed to calculate the wait time
 			
 			// TODO: we should verify the value of doRound.  When it returns
 			// -1 it can't communicate with the TS-BAT3
-			doRound(fd, &state, current, logfile);
-			if (!((samples == -1) && (cntsamples>10000))) cntsamples++;
+			doRound(fd, &state, params);
+			if (!((params->samples == -1) && (cntsamples>10000))) cntsamples++;
 			
 			
-			if (read && cntsamples==3) {
-				logabba(L_MIN, "Reading from address %i: %04X=%04X", 
-						address, state.ee_addr, state.ee_data);
+
+			if (params->read && cntsamples==3) {
+				logabba(L_MIN, "Reading from address %i: %04X=%04X",
+						params->address, state.ee_addr, state.ee_data);
 			}
 			
 			// checking switched states
 			if (state.batRun != prevstate.batRun) {
-				logabba(L_MIN, "Running on %s", 
+				logabba(L_MIN, "Running on %s",
 						state.batRun==ON?"batteries":"current" );
 			}
 			
 			if (state.softJP3 != prevstate.softJP3) {
-				logabba(L_MIN, "Soft JP3 switched to %s after %i samples", 
+				logabba(L_MIN, "Soft JP3 switched to %s after %i samples",
 						print_onoff(state.softJP3), cntsamples );
 			}
 			
@@ -365,11 +324,91 @@ int main(int argc, char *argv[]) {
 		val = 25 * 1000 - ( now.tv_sec - lastrun.tv_sec ) * 1000 * 1000 -  ( now.tv_usec - lastrun.tv_usec );
 		if (val <= 0 ) val = 0;
 		usleep(val);
-
+		
 	}
 	
-	close(fd);
 	closeMySocket(socketfd);
+	close(fd);
+	
+	return E_OKAY;
+}
+
+/*
+ * doRound creates a structure based on the current state found in sample
+ * and writes the result to the TS-BAT3 which should return a new sample.
+ * TODO: it should also contain the reading and writing to eeprom.
+ */
+int doRound(int fd, struct bat3 *sample, struct action *params) {
+	
+	struct bat3 state = *sample;
+	int cnt;
+	char msg[56];
+	
+	
+	logabba(L_INFO, "Original led: %s", print_onoff(state.led));
+	// Even when running on batteries, we have to kietel the opamp
+	// TODO: is this still correct?  I think we just have to write
+	// some info to the battery within 2 minutes and wait for reaction.
+	if (state.batRun == ON) {
+		state.led = OFF;
+		doload(&state, params);
+	} else {
+		changeled(&state);
+		// TODO: we should pass more info about the current loading state
+		// to the battery
+		doload(&state, params);
+		logabba(L_NOTICE, "Switching led to %s", print_onoff(state.led));
+	}
+	
+	logabba(L_INFO, "will encode led: %s", print_onoff(state.led));
+	
+	if ((cnt = encodemsg(msg, sizeof(msg), &state))) {
+		logabba(L_INFO, "Writing msg");
+		writeStream(fd, msg, cnt);
+	}
+	
+	cnt=0;
+	
+	while (!getsample(fd, &state, params->logfile) && cnt<MAX_RETRIES) {
+		cnt++;
+	}
+	
+	if (cnt>=MAX_RETRIES) {
+		logabba(L_MIN, "Did not get a sample even after retrying %d times", cnt);
+		return -1;
+	}
+	
+	logabba(L_INFO, "got state led: %s", print_onoff(state.led));
+	*sample = state;
+	logabba(L_INFO, "got sample led: %s", print_onoff(sample->led));
+	
+	return 0;
+	
+}
+
+int main(int argc, char *argv[]) {
+	
+	
+	
+	initMySocket();
+	initParams(&params);
+	processArguments(argc, argv, &params);
+	setPortno(params.portno);
+	
+	if ((params.samples<-1)) params.samples = DEFAULT_SAMPLES;
+	
+	setloglevel(params.loglevel, "bat3");
+	
+	#ifndef REV
+	logabba(L_MIN, "%s (Ver %s) started, loglevel %i, getting %d samples, using %imA to load, listening to port %d",
+			argv[0], VERSION, params.loglevel, params.samples, params.current, params.portno);
+	#else
+	logabba(L_MIN, "%s (Rev %s) started, loglevel %i, getting %d samples, using %imA to load, listening to port %d",
+			argv[0], REV, params.loglevel, params.samples, params.current, params.portno);
+	#endif
+	runArgument(&params);
+	
+	
 	
 	return 0;
 	
--- trunk/src/bat3func.c
+++ trunk/src/bat3func.c
@@ -20,206 +20,300 @@
 
 struct bat3 curState;
 
-
-typedef enum  {
-	WRITE,
-	LISTEN,
-	DONE
+typedef enum {
+    WRITE,
+    LISTEN,
+    DONE
 } romstate_t;
-romstate_t romstate=WRITE;
-
+romstate_t romstate = WRITE;
 
 void changeled(struct bat3* state) {
-	if (state->led == ON) {
-		logabba(L_NOTICE, "changeled: switching LED OFF");
-		state->led = OFF;
-	} else {
-		logabba(L_NOTICE, "changeled: switching LED ON");
-		state->led = ON;
-		
-	}
+    if (state->led == ON) {
+        logabba(L_NOTICE, "changeled: switching LED OFF");
+        state->led = OFF;
+    } else {
+        logabba(L_NOTICE, "changeled: switching LED ON");
+        state->led = ON;
+
+    }
 }
 
 static void switch_opamp(struct bat3* state, onoff_t sw) {
-	
-	state->opampEn  = sw;
-	state->PWM1en   = sw;
-	state->PWM2en   = sw;
-	
+
+    state->opampEn = sw;
+    state->PWM1en = sw;
+    state->PWM2en = sw;
+
 }
 
-void doload(struct bat3* state, int target) {
-	
-	int diff;
-	int newpwm;
-	int freese = 0;
-	// int pwmdiff;
-	
-	
-	if (state->batRun == ON) {
-		
-		if (state->softJP3 == ON) {
-			logabba(L_MIN, "Switching softJP3 OFF, softjp3=%s",
-					print_onoff(state->softJP3));
-			state->softJP3 = OFF;
-		}
-		logabba(L_INFO, "Running on batt...");
-		switch_opamp(state, OFF);
-		state->buckEn = OFF;
-		state->offsetEn = OFF;
-		state->updated = time(NULL);
-		
-		
-	} else {
-		
-		if (state->softJP3 == ON) {
-			logabba(L_MIN, "Switching softJP3 OFF, softjp3=%s", print_onoff(state->softJP3));
-			state->softJP3 = OFF;
-		}
-		
-		logabba(L_NOTICE, "NOT running on batt...");
-		
-		diff = time(NULL) - state->updated;
-		if (( diff > 30*60 ) && (diff < 12*60*60 ))  {
-			if (state->opampEn  == ON) logabba(L_MIN, "I stop loading");
-			switch_opamp(state, OFF);
-		} else {
-			if (state->opampEn  == OFF) logabba(L_MIN, "I start loading");
-			switch_opamp(state, ON);
-			
-			state->buckEn   = OFF;
-			state->offsetEn = OFF;
-			state->pwm_t    = MAX_PWM_T;
-			
-			logabba(L_NOTICE, "Doload: target=%imA, current=%imA" , target, battI(state->bat_i)/1000);
-			
-			diff = target - battI(state->bat_i)/1000;   // difference between wanted & current
-			// pwmdiff = abs(state->pwm_t - state->pwm_lo)/2; // if difference is big enough, we change it logarithmic
-			// if (pwmdiff < 2 ) pwmdiff = 2;
-			
-			newpwm = state->pwm_lo;
-			
-			if (diff>100) {
-				// newpwm -= pwmdiff);
-				newpwm -= 10;
-				logabba(L_NOTICE, "Doload: diff>100, newpwm = %04X", newpwm);
-			} else if (diff<-100) {
-				// newpwm += pwmdiff;
-				newpwm += 10;
-				logabba(L_NOTICE, "Doload: diff<-100, newpwm = %04X", newpwm);
-			} else if (diff>50) {
-				newpwm -= 1;
-			} else if (diff<-50) {
-				newpwm += 1;
-			}
-			
-			// preventing going to low and overcharge battery.
-			if ( target == DEFTARGET) {
-				if (newpwm < MIN_PWM_T) {
-					freese = 1;
-					newpwm = MIN_PWM_T;
-				}
-			} else {
-				// this one is dangerous
-				if (newpwm < 0) {
-					newpwm = 0;
-					freese = 1;
-				}
-				
-			}
-			
-			// you can't go highter than the top
-			if (newpwm>state->pwm_t) newpwm = state->pwm_t;
-			
-			logabba(L_INFO,  "Changing pwm_lo from %04X to %04X", state->pwm_lo, newpwm);
-			if (!freese) state->updated = time(NULL);
-			state->pwm_lo = newpwm;
-			
-		}
-		
-	}
+static void reset_history(struct action* target) {
+    // reset history timers
+    target->alarm = 0;
+    target->stable_time = 0;
+    target->weightpos = 0;
+}
+
+static int calc_load(struct bat3* state, struct action* target) {
+
+    int diff;
+    int newpwm;
+
+    diff = target->current - battI(state->bat_i) / 1000; // difference between wanted & current
+
+    // based on the current version
+    newpwm = state->pwm_lo;
+
+    if (diff > 100) {
+        // newpwm -= pwmdiff);
+        newpwm -= 10;
+        logabba(L_NOTICE, "Doload: diff>100, newpwm = %04X", newpwm);
+    } else if (diff<-100) {
+        // newpwm += pwmdiff;
+        newpwm += 10;
+        logabba(L_NOTICE, "Doload: diff<-100, newpwm = %04X", newpwm);
+    } else if (diff > 50) {
+        newpwm -= 1;
+    } else if (diff<-50) {
+        newpwm += 1;
+    }
+
+    // preventing going to low and overcharge battery.
+    if (target->current == DEFTARGET) {
+        if (newpwm < MIN_PWM_T) {
+
+            newpwm = MIN_PWM_T;
+        }
+    } else {
+        // this one is dangerous
+        if (newpwm < 0) {
+            newpwm = 0;
+
+        }
+
+    }
+
+    // you can't go highter than the top
+    if (newpwm > state->pwm_t) newpwm = state->pwm_t;
+
+    logabba(L_INFO, "Changing pwm_lo from %04X to %04X", state->pwm_lo, newpwm);
+
+    state->pwm_lo = newpwm;
+
+}
+
+static void process_first_degree(struct action* target, int newval) {
+
+    int pos;
+    float newweight;
+
+    // calculate the new weight
+    logabba(L_INFO, "process will add avg=%d", newval);
+
+    if (!(target->weightpos)) {
+        newweight = newval;
+    } else {
+        newweight =
+                1.00 * newval * (1 - FILTER) +
+                (target->weight[(target->weightpos) - 1]) * (FILTER);
+        logabba(L_NOTICE, "New weight (pos=%d): %d*(1-FILTER)+(%0.2f*(FILTER))=%0.2f",
+                (target->weightpos) - 1,
+                newval,
+                target->weight[(target->weightpos) - 1],
+                newweight);
+
+    }
+
+    // this should be the new position
+    // but we need to verify if there's still if there's  still room.
+    target->weightpos += 1;
+    if ((target->weightpos) == MAXMIN) {
+        // the end, shift the values!
+        for (pos = 1; pos < MAXMIN; pos++) target->weight[pos - 1] = target->weight[pos];
+        target->weightpos -= 1;
+    }
+
+    // bring them together: new value on the correct place
+    logabba(L_INFO, "New weight: %0.2f", newweight);
+    target->weight[target->weightpos - 1] = newweight;
+
+}
+
+int check_stable(struct action *target) {
+    // checks if we're loading stable
+    float curr, prev, diff;
+
+    // preventing buffer overflow
+    if (target->weightpos < 2) return 0;
+
+    curr = target->weight[(target->weightpos) - 1];
+    prev = target->weight[(target->weightpos) - 2];
+
+    diff = curr - prev;
+    logabba(L_NOTICE, "Difference in weight (weightpos=%d): %0.2f - %0.2f = %0.2f",
+            target->weightpos, curr, prev, diff);
+    if ((diff > -1) && (diff < 1)) {
+        // running stable
+        if (target->stable_time == 0) {
+            logabba(L_MIN, "Starting to run almost stable now, waiting 15min");
+            time(&(target->stable_time));
+            return 0;
+        } else {
+            if ((diff = time(NULL) - target->stable_time) > 15 * 60) { // 15 times 60 seconds
+                if ((diff < 15 * 60 + 2))
+                    logabba(L_MIN, "Running stable now for at least 15 min");
+                return 1;
+            }
+            return 0;
+        }
+
+    } else {
+        // not running stable
+        target->stable_time = 0;
+        return 0;
+    }
+}
+
+void doload(struct bat3* state, struct action* target) {
+
+    int minpos;
+    int stable = 0;
+
+    if (state->batRun == ON) {
+        // Running on batteries
+        logabba(L_NOTICE, "Running on batt...");
+
+        if (state->softJP3 == ON) {
+            logabba(L_MIN, "Batrun, switching softJP3 OFF, softjp3=%s",
+                    print_onoff(state->softJP3));
+            state->softJP3 = OFF;
+        }
+
+        switch_opamp(state, OFF);
+        state->buckEn = OFF;
+        state->offsetEn = OFF;
+        state->updated = time(NULL);
+
+        reset_history(target);
+
+    } else { // Not running on batteries
+
+        logabba(L_NOTICE, "NOT running on batt...");
+
+        if (state->softJP3 == ON) {
+            logabba(L_MIN, "No batrun, Switching softJP3 OFF, softjp3=%s",
+                    print_onoff(state->softJP3));
+            state->softJP3 = OFF;
+        }
+
+        if (target->alarm <= time(NULL)) { // time to record the setting
+            // reset the alarm;
+            time(&(target->alarm));
+            target->alarm += 1; // next second we will record again
+            if (!check_stable(target)) {
+                // If previous round didn't give us a stable result we try again
+                // otherwise we should wait until time has passed
+                process_first_degree(target, state->bat_u);
+            }
+        }
+
+        if (check_stable(target)) {
+            logabba(L_MIN, "Seem to be stable, switching off");
+            switch_opamp(state, OFF);
+        } else {
+            calc_load(state, target);
+
+            if (state->opampEn == OFF) logabba(L_MIN, "I start loading");
+            switch_opamp(state, ON); // should only be done if target!=0
+
+            state->buckEn = OFF;
+            state->offsetEn = OFF;
+            state->pwm_t = MAX_PWM_T;
+        }
+
+    }
+
 }
 
 void doread(struct bat3* state, int address) {
-	
-	logabba(L_MIN, "doread: reading from address = %04X", address);
-	
-	if (romstate == WRITE) {
-		if ((state->ee_addr == address) && (state->ee_read == ON)) {
-			romstate = LISTEN;
-		} else {
-			state->ee_addr  = address;
-			state->ee_read  = ON ;
-			state->ee_write = OFF;
-			state->ee_data  = 0x00; // don't know if we need this
-		}
-	}
-	
-	if (romstate == LISTEN) {
-		if ((state->ee_addr == address) && (state->ee_read == OFF)) {
-			romstate = DONE;
-		} else {
-			state->ee_addr  = address;
-			state->ee_read  = OFF;
-			state->ee_write = OFF;
-		}
-	}
-	
-	logabba(L_MIN, "doread: addr=%04X, read=%s, write=%s, data=%04X",
-			state->ee_addr, print_onoff(state->ee_read), print_onoff(state->ee_write), state->ee_data);
-	
-	
+
+    logabba(L_MIN, "doread: reading from address = %04X", address);
+
+    if (romstate == WRITE) {
+        if ((state->ee_addr == address) && (state->ee_read == ON)) {
+            romstate = LISTEN;
+        } else {
+            state->ee_addr = address;
+            state->ee_read = ON;
+            state->ee_write = OFF;
+            state->ee_data = 0x00; // don't know if we need this
+        }
+    }
+
+    if (romstate == LISTEN) {
+        if ((state->ee_addr == address) && (state->ee_read == OFF)) {
+            romstate = DONE;
+        } else {
+            state->ee_addr = address;
+            state->ee_read = OFF;
+            state->ee_write = OFF;
+        }
+    }
+
+    logabba(L_MIN, "doread: addr=%04X, read=%s, write=%s, data=%04X",
+            state->ee_addr, print_onoff(state->ee_read), print_onoff(state->ee_write), state->ee_data);
+
+
 }
 
 void dowrite(struct bat3* state, int address, int value) {
-	
-	logabba(L_MIN, "dowrite: writing %04X to address = %04X", value, address);
-	
-	
-	if (romstate == WRITE) {
-		if ((state->ee_addr == address) && (state->ee_write == ON)) {
-			romstate = LISTEN;
-		} else {
-			state->ee_addr  = address;
-			state->ee_read  = OFF ;
-			state->ee_write = ON;
-			state->ee_data  = value; // don't know if we need this
-		}
-	}
-	
-	if (romstate == LISTEN) {
-		if ((state->ee_addr == address) && (state->ee_write == OFF)) {
-			romstate = DONE;
-		} else {
-			state->ee_addr  = address;
-			state->ee_read  = OFF;
-			state->ee_write = OFF;
-		}
-	}
-	
-	logabba(L_MIN, "dowrite: addr=%04X, read=%s, write=%s, data=%04X",
-			state->ee_addr, print_onoff(state->ee_read), print_onoff(state->ee_write), state->ee_data);
-	
-	
+
+    logabba(L_MIN, "dowrite: writing %04X to address = %04X", value, address);
+
+
+    if (romstate == WRITE) {
+        if ((state->ee_addr == address) && (state->ee_write == ON)) {
+            romstate = LISTEN;
+        } else {
+            state->ee_addr = address;
+            state->ee_read = OFF;
+            state->ee_write = ON;
+            state->ee_data = value; // don't know if we need this
+        }
+    }
+
+    if (romstate == LISTEN) {
+        if ((state->ee_addr == address) && (state->ee_write == OFF)) {
+            romstate = DONE;
+        } else {
+            state->ee_addr = address;
+            state->ee_read = OFF;
+            state->ee_write = OFF;
+        }
+    }
+
+    logabba(L_MIN, "dowrite: addr=%04X, read=%s, write=%s, data=%04X",
+            state->ee_addr, print_onoff(state->ee_read), print_onoff(state->ee_write), state->ee_data);
+
+
 }
 
 void setBatState(struct bat3* newState) {
-	curState = *newState;
+    curState = *newState;
 }
 
 onoff_t getBatRun() {
-	
-	return curState.batRun;
+
+    return curState.batRun;
 }
 
 int getBatI() {
-	return curState.bat_i;
+    return curState.bat_i;
 }
 
 int getAddress() {
-	return -1;
+    return -1;
 }
 
 struct bat3* getState() {
-	return &curState;
+    return &curState;
 }
